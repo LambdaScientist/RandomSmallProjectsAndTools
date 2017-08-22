959179cc0f37deab6a0d1a5d8455bf1c18cddca5
LambdaScientist <nothing.just.a@gmail.com>
2017-08-22 10:16:02 -0500

    Add files via upload

---
+++ oldVHDLParsers/vhdlParser/testVhdl.vhd
@@ -0,0 +1,19 @@
+-- (this is a VHDL comment)
+
+-- import std_logic from the IEEE library
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+-- this is the entity
+entity ANDGATE is
+  port ( 
+    I1 : in std_logic;
+    I2 : in std_logic;
+    O  : out std_logic);
+end entity ANDGATE;
+
+-- this is the architecture
+architecture RTL of ANDGATE is
+begin
+  O <= I1 and I2;
+end architecture RTL;
